cd660299975df2840c448df7beb5558f44dd3fff
Paolo Angeli <pangeli70@gmail.com>
2022-09-17 15:41:28 +0200

    First commit

---
+++ deps.ts
@@ -0,0 +1,9 @@
+/** -----------------------------------------------------------------------
+ * @module [Uts]
+ * @author [APG] ANGELI Paolo Giusto
+ * ------------------------------------------------------------------------
+ */
+
+// https://deno.land/std
+export * as StdFs from "https://deno.land/std@0.153.0/fs/mod.ts";
+export * as StdPath from "https://deno.land/std@0.153.0/path/mod.ts";
