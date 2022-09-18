db4b5c821ec2d716443c9081e78c819221421403
Paolo Angeli <pangeli70@gmail.com>
2022-09-18 09:53:41 +0200

    Chore inverted chars

--- src/classes/ApgUts.ts
+++ src/classes/ApgUts.ts
@@ -712,15 +712,15 @@ export class ApgUtsServer {
     static StartupResume(info: IApgUtsServerInfo) {
         const now = new Date().toISOString();
         console.log(`\n\n\n`)
-        console.log(ApgUtsStr.FilledCentered('', 80, "-", "+"))
+        console.log(ApgUtsStr.FilledCentered('', 80, "+", "-"))
         console.log(ApgUtsStr.FilledCentered(info.name, 80, "|", " "))
         console.log(ApgUtsStr.FilledCentered(info.title, 80, "|", " "))
         console.log(ApgUtsStr.FilledCentered(info.subtitle, 80, "|", " "))
         console.log(ApgUtsStr.FilledCentered(`${now} (ISO)`, 80, "|", " "))
         console.log(ApgUtsStr.FilledCentered(`http://localhost:${info.localPort}`, 80, "|", " "))
         console.log(ApgUtsStr.FilledCentered(`Drash Server ready to receive request`, 80, "|", " "))
-        console.log(ApgUtsStr.FilledCentered('', 80, "-", "+"))
+        console.log(ApgUtsStr.FilledCentered('', 80, "+", "-"))
         console.log(`\n\n\n`)
     }
-    
+
 }
