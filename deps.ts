b70d9b60ab05e83657ab5cbbfc32463fb4817d91
Paolo Angeli <pangeli70@gmail.com>
2022-09-19 01:52:18 +0200

    Merging back errors

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 /** -----------------------------------------------------------------------
- * @module [Uts]
+ * @module [Mng]
  * @author [APG] ANGELI Paolo Giusto
  * ------------------------------------------------------------------------
  */
@@ -7,3 +7,26 @@
 // https://deno.land/std
 export * as StdFs from "https://deno.land/std@0.153.0/fs/mod.ts";
 export * as StdPath from "https://deno.land/std@0.153.0/path/mod.ts";
+
+// https://deno.land/x/mongo
+export {
+    Bson,
+    MongoClient,
+    Database as MongoDatabase,
+    Collection as MongoCollection
+} from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+
+export type {
+    FindOptions,
+    CountOptions,
+    ConnectOptions
+} from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+
+//deno.land/x/web_bson
+export {
+    ObjectId as BsonObjectID
+} from 'https://deno.land/x/web_bson@v0.1.10/src/bson.ts';
+
+// https://github
+export * as Uts from "https://raw.githubusercontent.com/Pangeli70/apg-uts/master/mod.ts";
+export * as Rst from "https://raw.githubusercontent.com/Pangeli70/apg-rst/master/mod.ts";
