535f14e2570bc215f8b9b2bb7d67523e15c3d80b
PAngeli70 <pangeli70@gmail.com>
2023-04-24 02:42:06 +0200

    Aligned to Apg-Dir entries

--- src/classes/ApgUtsServer.ts
+++ src/classes/ApgUtsServer.ts
@@ -2,6 +2,7 @@
  * @module [Uts]
  * @author [APG] ANGELI Paolo Giusto
  * @version 0.9.1 [APG 2022/09/11] Github Beta
+ * @version 0.9.6 [APG 2023/04/24] Modified fileds to match Apg-Dir entry
  * -----------------------------------------------------------------------
 */
 
@@ -14,10 +15,10 @@ export class ApgUtsServer {
     const now = new Date().toISOString();
     console.log(`\n\n\n`);
     console.log(ApgUtsStr.FilledCentered('', 80, "+", "-"));
-    console.log(ApgUtsStr.FilledCentered(info.name, 80, "|", " "));
+    console.log(ApgUtsStr.FilledCentered(info.caption, 80, "|", " "));
     console.log(ApgUtsStr.FilledCentered(info.title, 80, "|", " "));
-    console.log(ApgUtsStr.FilledCentered(info.subtitle, 80, "|", " "));
-    console.log(ApgUtsStr.FilledCentered(`${now} (ISO)`, 80, "|", " "));
+    console.log(ApgUtsStr.FilledCentered(`Version ${info.version}`, 80, "|", " "));
+    console.log(ApgUtsStr.FilledCentered(`Started ${now} (ISO)`, 80, "|", " "));
     console.log(ApgUtsStr.FilledCentered(`http://localhost:${info.localPort}`, 80, "|", " "));
     console.log(ApgUtsStr.FilledCentered(`Drash Server ready to receive requests`, 80, "|", " "));
     console.log(ApgUtsStr.FilledCentered('', 80, "+", "-"));
--- src/interfaces/IApgUtsServerInfo.ts
+++ src/interfaces/IApgUtsServerInfo.ts
@@ -2,13 +2,18 @@
  * @module [Uts]
  * @author [APG] ANGELI Paolo Giusto
  * @version 0.9.1 [APG 2022/09/11] Github Beta
+ * @version 0.9.6 [APG 2023/04/24] Removed subtitle to match Apg-Dir entry
  * -----------------------------------------------------------------------
 */
 
+/** Brief set of data for Drash server startup and resume*/
 export interface IApgUtsServerInfo {
-  name: string;
+  /** Name of the server */
+  caption: string;
+  /** Brief description */
   title: string;
-  subtitle: string;
+  /** Current version */
+  version: string;
+  /** Local development port */
   localPort: number;
-
 }
